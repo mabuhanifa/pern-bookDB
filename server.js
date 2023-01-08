e4dd6afad84110b8a938dea3be1275eec4523b1b
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-08 23:55:56 +0600

    db

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Application Working");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
