86d9b6b2657f95f3ae1628ec9c735aae0b2cc955
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-09 04:09:30 +0600

    app.put

--- server.js
+++ server.js
@@ -34,6 +34,19 @@ app.get("/books/:id", async (req, res) => {
   }
 });
 
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM book WHERE id=$1", [id]);
+    res.status(201).json({
+      message: `successfully deleted book ${id}`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: error.message });
+  }
+});
+
 app.post("/books", async (req, res) => {
   try {
     const { name, description } = req.body;
@@ -46,6 +59,25 @@ app.post("/books", async (req, res) => {
     res.status(201).json({
       message: `book was created successfully ${id} ,${name}, ${description}`,
       data: newBook.rows,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: error.message });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const updatedBook = await pool.query(
+      "UPDATE book SET name=$1, description=$2 WHERE id=$3 RETURNING *",
+      [name, description, id]
+    );
+
+    res.status(201).json({
+      message: `book was updated successfully ${id} ,${name}, ${description}`,
+      data: updatedBook.rows,
     });
   } catch (error) {
     console.error(error);
