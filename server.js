af46a20ecad4123293928d6552408190743aa9f3
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-09 00:34:58 +0600

    app.post

--- server.js
+++ server.js
@@ -6,8 +6,32 @@ app.use(express.json());
 app.use(cors());
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Application Working");
+app.get("/books", async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    res
+      .status(201)
+      .json({
+        message: `book was created successfully ${name} ${description}`,
+      });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
